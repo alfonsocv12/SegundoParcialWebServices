29a65b29926bdfb55a4698bed7b3a9decf6a0fa0
alfonsocv12 <alfonsocvu@gmail.com>
2020-03-26 17:11:54 -0700

    Initial Commit

---
+++ SegundoParcialWebServices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegundoParcialWebServices
+//
+//  Created by apple on 3/26/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
